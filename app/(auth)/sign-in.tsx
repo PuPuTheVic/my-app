010db27da0b2a37a24b16619c3038619b0dab857
PuPuTheVic <hanhtoon9@gmail.com>
2026-08-18 12:15:44 +0630

    feat: refactor authentication and tab components to use SafeAreaView and add theme constants

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,13 +1,17 @@
 import { Link } from "expo-router";
+import { styled } from "nativewind";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafeAreaView);
 
 const SignIn = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1 p-5 bg-background">
       <Text>SignIn</Text>
       <Link href="/(auth)/sign-up">Don&apos;t have an account? Sign up</Link>
-    </View>
+    </SafeAreaView>
   );
 };
 
